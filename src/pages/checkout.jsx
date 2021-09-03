2e7c20d8182553cdebb29c038eb0f697157f17c9
IsmatAyash <ismat.ayash@gmail.com>
2021-09-03 22:35:22 +0300

    corrected publishable key

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -5,7 +5,9 @@ import { Elements } from "@stripe/react-stripe-js"
 import CheckoutForm from "../components/CheckoutForm"
 
 const Checkout = () => {
-  const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
+  const stripePromise = loadStripe(
+    "pk_test_51HRNBKE41CMIaPCOspVA8HNH9z6mLgmSIh60NeR6WIdz3Zt3GR3HyMNOY6KJG8TdEYDX9Rz8EKb2MyydfRzHCQoH00geFUH06W"
+  )
   return (
     <section className="checkout-wrapper">
       <Elements stripe={stripePromise}>
